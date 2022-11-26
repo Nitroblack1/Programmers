42ace3625515c6b140e46de9f34c1647f184e906
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-27 02:12:12 +0900

    no message

--- src/main/java/programmers/Lv0_problems/Bacteria.java
+++ src/main/java/programmers/Lv0_problems/Bacteria.java
@@ -1,5 +1,9 @@
 package programmers.Lv0_problems;
 
+import programmers.Main;
+
 public class Bacteria {
-    
+    public static int solution(int n, int t) {
+        return (int)(n*Math.pow(2, t));
+    }
 }
--- src/main/java/programmers/Main.java
+++ src/main/java/programmers/Main.java
@@ -1,7 +1,15 @@
 package programmers;
 
+import programmers.Lv0_problems.Bacteria;
+
 public class Main {
 
     public static void main(String[] args) {
+        int n1 = 2;
+        int t1 = 10;
+        int n2 = 7;
+        int t2 = 15;
+        System.out.println(Bacteria.solution(n1, t1));
+        System.out.println(Bacteria.solution(n2, t2));
     }
 }
