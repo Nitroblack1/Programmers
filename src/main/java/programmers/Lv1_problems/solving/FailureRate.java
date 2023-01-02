bc40ce8d1e5e24a8e11b6ec1ce0e39870a9bf02b
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2023-01-02 18:41:35 +0900

    solving problem - Failure Rate

--- src/main/java/programmers/Lv1_problems/solving/FailureRate.java
+++ src/main/java/programmers/Lv1_problems/solving/FailureRate.java
@@ -1,7 +1,35 @@
 package programmers.Lv1_problems.solving;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class FailureRate {
     public int[] solution(int N, int[] stages) {
-        return new int[]{3};
+        double[][] failureRate = new double[N][2];
+        int[] answer = new int[5];
+        int usersNum = stages.length;
+        int unclearedUsersNum;
+
+        for(int i = 1; i <= N; i++) {
+            unclearedUsersNum = 0;
+            for(int stage : stages) {
+                if (stage == i) {
+                    unclearedUsersNum++;
+                }
+            }
+            failureRate[i-1][0] = (double) unclearedUsersNum / usersNum;
+            failureRate[i-1][1] = i;
+            usersNum -= unclearedUsersNum;
+        }
+        System.out.println(Arrays.deepToString(failureRate));
+        Arrays.sort(failureRate, Comparator.comparingDouble((double[] o) -> o[0]).reversed());
+        for(int i = 0; i < failureRate.length - 1; i++) {
+            if (failureRate[i][1] == failureRate[i+1][1]) {
+                
+            }
+        }
+        System.out.println(Arrays.deepToString(failureRate));
+        return new int[0];
     }
 }
